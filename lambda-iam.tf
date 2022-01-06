8b834eb61829f5e4857c76eaaadd7dcfe083690a
desaiprasad87 <97249577+desaiprasad87@users.noreply.github.com>
2022-01-06 23:40:21 +0530

    Add files via upload

---
+++ lambda-iam.tf
@@ -0,0 +1,13 @@
+	
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = "$(aws_iam_role.lambda_role.id}"
+  
+  policy = "${file("C:/Users/SAIPRASAD/Downloads/terraform/terra_lambda/IAM/lambda-policy.json")}"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = "${file("/IAM/lambda-assume-policy.json")}"
+}
